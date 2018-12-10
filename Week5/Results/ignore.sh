0f5e8015f7de3db6e45cdf40ce140e1e94a1c619
Wang YuHeng <yuheng.wang18@imperial.ac.uk>
2018-12-10 12:16:34 +0000

    test

--- Week5/Results/ignore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Author: Wang YuHeng yuheng.wang18@imperial.ac.uk
-# Script: ignore
-# Desc: ignore file larger than 35mb in results
-# Argument: none
-# Date: Dec 2018 
-
-find . -size +35M | cat >> .gitignore
